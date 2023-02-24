854a9bd066d2b4838a13acfc2fce5052e58d56b6
Priyanka Bhatta <priyankabhatta2323@gmail.com>
2023-02-24 18:18:12 +0545

    Changes in UI

--- malikarjun/urls.py
+++ malikarjun/urls.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from main import views
+from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
